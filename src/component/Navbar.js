654579dd20673de60056d68ee3abc08b70f27f5e
bcz3 <131152121+bcz3@users.noreply.github.com>
2023-04-19 14:35:38 +0700

    update src/component/Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,34 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-
-  useEffect(() => {
-    const bodyEl = document.querySelector("#body");
-    const menuBtnEl = document.querySelector(".nav__toggle-menu");
-    const closeBtnEl = document.querySelector(".nav__toggle-close");
-    const menuContainerEl = document.querySelector("#nav-menu");
-    const menuLinkEl = document.querySelector("#nav-list");
-  
-    const toggleMenu = () => {
-      if (window.innerWidth <= 767) {
-        bodyEl.classList.toggle("ovh");
-        menuBtnEl.classList.toggle("show-icon");
-        closeBtnEl.classList.toggle("show-icon");
-        menuContainerEl.classList.toggle("show-menu");
-      }
-    };
-  
-    menuBtnEl.addEventListener("click", toggleMenu);
-    closeBtnEl.addEventListener("click", toggleMenu);
-    menuLinkEl.addEventListener("click", toggleMenu);
-
-    return () => {
-      menuBtnEl.removeEventListener("click", toggleMenu);
-      closeBtnEl.removeEventListener("click", toggleMenu);
-      menuLinkEl.removeEventListener("click", toggleMenu);
-    };
-  }, []);
 
   return (
     <header class="header">
