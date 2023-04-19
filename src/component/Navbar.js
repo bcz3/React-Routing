29e0187f92f1d78969d8deb609df201b1a52bf05
bcz3 <131152121+bcz3@users.noreply.github.com>
2023-04-19 14:11:27 +0700

    update src/component/Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,21 +3,37 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav>
-      <h1>Abdillah</h1>
-        <ul>
-          <NavLink to={"/"}>
-            <li>Home</li>
-          </NavLink>
-          <NavLink to={"/about"}>
-            <li>About</li>
-          </NavLink>
-          <NavLink to={"/contact"}>
-            <li>Contact</li>
-          </NavLink>
-        </ul>
-    </nav>
-  );
+<header class="header">
+  <nav class="nav">
+    <div class="nav__data">
+      <NavLink to={"/"}>
+        <img src="logon.png" alt="The logo"/>
+      </NavLink>
+      <div class="nav__toggle">
+        <button class="nav__toggle-menu" id="toggle-menu">
+          <span></span>
+        </button>
+        <button class="nav__toggle-close" id="toggle-close">
+          <span></span>
+        </button>
+      </div>
+    </div>
+    <div class="nav__menu" id="nav-menu">
+      <ul class="nav__list" id="nav-list">
+        <NavLink to={"/"}>
+          <li><a href="#" class="nav__link">Home</a></li>
+        </NavLink>
+        <NavLink to={"/about"}>
+          <li><a href="#" class="nav__link">About</a></li>
+        </NavLink>
+        <NavLink to={"/contact"}>
+          <li><a href="#" class="nav__link">Contact</a></li>
+        </NavLink>
+      </ul>
+    </div>
+  </nav>
+</header>
+    );
 }
 
 export default Navbar
